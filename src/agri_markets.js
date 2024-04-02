56a47b0b0cefc127171b0a0ddbf31fa0039cbdac
Astero-H <ahhennane@gmail.com>
2024-04-02 02:51:51 +0200

    Supplying organized data to chart.js

--- src/agri_markets.js
+++ src/agri_markets.js
@@ -1,34 +1,55 @@
 import { Chart } from "chart.js/auto";
-import { getAgriData } from "./agridata.js";
+import { getAgriData, getChartData } from "./agridata.js";
+const bootstrap = require('bootstrap');
+
+let myChart = null;
 
 (async function () {
-  const chartData = await getAgriData();
+  await getAgriData(); // Initialisation des boutons et écouteurs d'événements
+})();
 
-  if (chartData) {
-    new Chart(document.getElementById("agridata"), {
-      type: "bar",
-      data: chartData,
-      options: {
-        animation: true,
-        plugins: {
-          legend: {
+let selectElement = document.getElementById('cat-product');
+selectElement.addEventListener('change', async function() {
+  const selectedProductIndex = this.value;
+
+  if (window.currentFileData) {
+    const chartData = getChartData(window.currentFileData, selectedProductIndex);
+    createChart(chartData);
+  } else {
+    console.error("Aucun fichier n'est sélectionné.");
+  }
+});
+
+function createChart(chartData) {
+  const ctx = document.getElementById("agridata").getContext('2d');
+
+  // Détruire le graphique existant s'il y en a un
+  if (myChart) {
+    myChart.destroy();
+  }
+
+  // Créer un nouveau graphique avec les données fournies
+  myChart = new Chart(ctx, {
+    type: "line",
+    data: chartData,
+    options: {
+      animation: true,
+      plugins: {
+        legend: {
             display: true,
-          },
-          tooltip: {
-            enabled: true,
-          },
         },
-        scales: {
-          y: {
-            beginAtZero: true,
-            ticks: {
-              callback: value => `${value} €/kg (HT)`
-            }
-          },
+        tooltip: {
+            enabled: true,
         },
       },
-    });
-  } else {
-    console.error("Impossible de charger les données du graphique.");
-  }
-})();
+      scales: {
+        y: {
+            beginAtZero: true,
+            ticks: {
+                callback: value => `${value} €/kg (HT)`
+            }
+        },
+      },
+    },
+  });
+}
