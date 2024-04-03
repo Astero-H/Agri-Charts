2e0aabbf76e85fc20b32de1b9d3bb5d60996fcae
Astero-H <ahhennane@gmail.com>
2024-04-04 01:33:39 +0200

    Fix URL for mongoose

--- config/database.js
+++ config/database.js
@@ -3,6 +3,6 @@ const mongoose = require('mongoose');
 const port = process.env.DB_PORT || 27017;
 const dbName = process.env.DB_NAME;
 
-mongoose.connect(`mongodb://localhost:${port}/${dbName}`)
+mongoose.connect(`mongodb://127.0.0.1:${port}/${dbName}`)
   .then(() => console.log('Connexion à MongoDB réussie.'))
   .catch(err => console.error('Erreur de connexion à MongoDB:', err));
